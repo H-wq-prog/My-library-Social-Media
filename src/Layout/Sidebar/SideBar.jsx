a5180fa10b3e6235df0836c396870d4a7545b3f8
Heidi Ramdan <heidiramdan48@gmail.com>
2026-05-02 17:54:08 +0300

    Delete src directory

--- src/Layout/Sidebar/SideBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './SideBar.module.css'
-
-export default function SideBar() {
-    return (
-        <div>SideBar</div>
-    )
-}
--- src/Pages/CommunityFeed.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function CommunityFeed() {
-    return (
-        <div>CommunityFeed</div>
-    )
-}
